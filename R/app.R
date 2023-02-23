b38c08085e74b4b0f578405f516d082cad2ed310
shateva <72813500+shateva@users.noreply.github.com>
2023-02-23 13:53:40 -0500

    Update app.R

--- R/app.R
+++ R/app.R
@@ -1,19 +1,57 @@
-#' @export
-app <- function(...) {
+library(shiny)
+library(gt)
+
+source(here::here("R/source_all.R"))
+
+app <- function() {
 
   ui <- fluidPage(
     titlePanel("DEI Student Survey"),
     tabsetPanel(
-      course_satisfaction_ui("course_satisfaction"),
-      course_corr_ui("course_corr"),
-      compare_groups_ui("compare_groups")
+      tabPanel(
+        "Course satisfication",
+        sidebarLayout(
+          sidebarPanel(
+            selectInput(
+              "course",
+              label = "Select courese(s)",
+              choices = unique(course_df$course),
+              multiple = TRUE
+            )
+          ),
+          mainPanel(
+            headerPanel("Course satisfication response distribution"),
+            plotOutput("freq_plot")
+          )
+        )
+      )
+      # compare_groups_ui("compare_groups")
     )
   )
 
   server <- function(input, output) {
-    course_satisfaction_server("course_satisfaction")
-    course_corr_server("course_corr")
-    compare_group_server("compare_groups")
+    output$freq_plot <- renderPlot(
+      {
+        if (length(input$course) > 0) {
+          g <- course_df %>%
+            filter(course %in% input$course) %>%
+            group_by(course) %>%
+            count(response, name = "count", .drop = FALSE) %>%
+            mutate(prop = count / sum(count)) %>%
+            ggplot(aes(x = response, fill = course, group = course))
+          title <- "Responses for selected course(s)"
+        } else {
+          g <- course_df %>%
+            count(response, name = "count", .drop = FALSE) %>%
+            mutate(prop = count / sum(count)) %>%
+            ggplot(aes(x = response, group = factor(1)))
+          title <- "All responses"
+        }
+        stack_freq_prop(g, title = title)
+      },
+      height = 600
+    )
+    # compare_group_server("compare_groups")
   }
 
   shinyApp(ui = ui, server = server)
